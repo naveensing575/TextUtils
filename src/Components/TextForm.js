9279c982396e99be91f5ba8561d7c6e63a369fc5
naveensing575 <naveensing575@gmail.com>
2022-01-29 11:19:13 +0530

    Text buttons added

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 function TextForm(props) {
+    const handleUpClick=()=>{
+        let newText= Text.toUpperCase();
+        setText(newText);
+    }
+    const handleLoClick=()=>{
+        let newText= Text.toLowerCase();
+        setText(newText);
+    }
+    const handleOnChange=(event)=>{
+        setText(event.target.value);
+    }
+    const [Text, setText] = useState('');
+
   return (
       <>
     <div className="mb-3">
-    <label for="myBox" class="form-label"><h3>{props.heading}</h3></label>
-    <textarea className="form-control" id="myBox" rows="3"></textarea>
+    <h3>{props.heading}</h3>
+    <textarea className="form-control" id="myBox" rows="3" value={Text} onChange={handleOnChange}></textarea>
     </div>
-    <button className="btn btn-primary">Convert to Uppercase</button>
+    <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+    <button className="btn btn-primary mx-3" onClick={handleLoClick}>Convert to Lowercase</button>
     </>
   );
 }
